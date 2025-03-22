a5e76d783f224ecb71f1a18602d16f61542d5cea
ThummarDarshan <darshantce.059@gmail.com>
2025-03-22 13:03:56 +0530

    Add files via upload

---
+++ odd&even_all.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the Number =";
+    cin>>n;
+
+          cout<<"Even Numbers are :-"<<endl;
+    for(int i=1;i<=n ;i++)
+      {
+          if(i%2==0)
+          {
+              cout<<i<<endl;
+          }
+      }
+
+    return 0;
+}
