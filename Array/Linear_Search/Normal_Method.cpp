a5f820ec8cb217f1b1ee6dc0613d24234621fddb
Vaghasiya Dhruvin <dhruvinvaghasiya297@gmail.com>
2025-03-22 15:26:09 +0530

    Create Normal_Method.cpp

---
+++ Array/Linear_Search/Normal_Method.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int Linear_Search(int arr[ ] , int size , int target )
+{
+    for (int i=0 ; i < size ;i++)
+    {
+        if(arr[i]==target)
+        {
+            return i; // if found the value then return the index of the value
+        }
+    }
+    return -1;//if not found in array then return -1 
+}
+
+int main ( )
+{
+    int arr[] = {4 ,42, 42, 23,42,45,21};
+    int target ;
+    cout << "Enter target : ";
+    cin >> target ;
+    int index;
+
+    index = Linear_Search(arr,7,target);
+    cout<<"The index is (if -1 then not found) : "<<index<<endl;
+    return 0;
+}
