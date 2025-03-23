af8146b7f58a462b7105c7ed13b9d214abe0eed4
ThummarDarshan <darshantce.059@gmail.com>
2025-03-23 16:55:22 +0530

    Add files via upload

---
+++ find_lawer_upper_with_asciivalur.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        char ch;
+        cout<<"Enter the character :- ";
+        cin>>ch;
+
+        if(ch>=65&& ch<=90)
+        {
+            cout<<"\n"<<"lowercase\n" ;
+        }
+        else
+        {
+            cout<<"uppercase\n";
+        }
+}
