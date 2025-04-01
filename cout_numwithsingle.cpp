4403818678157a0a9ccb997a3e84241f0681c6a9
ThummarDarshan <darshantce.059@gmail.com>
2025-04-01 18:55:40 +0530

    Add files via upload

---
+++ cout_numwithsingle.cpp
@@ -0,0 +1,31 @@
+
+
+   #include <iostream>
+using namespace std;
+
+int digit_sum(int n)
+{
+    int sum=0;
+    int num=n;
+    do
+    {
+        int r=num%10;
+        num=num/10;
+        sum += r;
+
+    } while (num>0);
+
+
+cout<<"sum of single digit = "<<sum;
+}
+
+int main()
+{
+    int n,sum;
+
+   cout<<"enter the number =";
+   cin>>n;
+   digit_sum(n);
+ return 0;
+
+}
